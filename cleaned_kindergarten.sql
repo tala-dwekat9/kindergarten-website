48a57fb1e9d2b6756a798e723031e3d464c32d62
Tala69 <taladwekat179@gmail.com>
2025-07-17 15:38:26 +0300

    ✅ Add IF NOT EXISTS to table creation in SQL file

--- cleaned_kindergarten.sql
+++ cleaned_kindergarten.sql
@@ -1,3 +1,3 @@
 -- This is a simulation of the user's dump with problematic lines
 CREATE DATABASE kindergarten_2025;
-CREATE TABLE test_table (id SERIAL PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS test_table(id SERIAL PRIMARY KEY);
